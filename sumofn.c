b9ecd1a4606c6fe4fa445496a5ece7665b2e6a5f
karthikeyan0197 <karthikeyan0197@gmail.com>
2016-12-23 14:48:29 +0530

    Add files via upload

---
+++ sumofn.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int sum=0,n,i;
+     printf("\n Enter the number to find till sum:\t");
+     scanf("%d",&n);
+     for(i=1;i<=n;i++)
+        sum=sum+i;
+
+     printf("%d",sum);
+     getch();
+}
