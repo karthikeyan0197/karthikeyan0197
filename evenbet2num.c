c1543cea078984a6b19c181e63e3facfcd7b6d6f
karthikeyan0197 <karthikeyan0197@gmail.com>
2016-12-26 10:01:30 +0530

    Add files via upload

---
+++ evenbet2num.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+    int i,j,k;
+    printf("\n Enter the 2 Intervals:\t");
+    scanf("%d %d",&i,&j);
+    for(k=i;k<=j;k++)
+    {
+        if(k%2==0)
+              printf("%d\t",k);
+
+    }
+}
+
