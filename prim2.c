74537c27f34377cd7cad13a4dc3317dd21d391c7
karthikeyan0197 <karthikeyan0197@gmail.com>
2016-12-28 10:23:04 +0530

    Add files via upload

---
+++ prim2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int n,a,flag = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+    if(n>0){
+    for(a=2;a<=n/2;++a)
+    {
+        if(n%a==0)
+        {
+            flag=1;
+            break;
+        }
+    }
+
+    if (flag==0)
+        printf("%d is a prime number.",n);
+    else
+        printf("%d is not a prime number.",n);
+
+    return 0;
+}}
